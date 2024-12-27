8807ec536d5dbf61688412c91c7900ed2bd462da
w4ngzhen <w4ngzhen@hotmail.com>
2024-12-27 10:01:28 +0800

    refactor: 调整编译脚本

--- build-script.sh
+++ build-script.sh
@@ -2,6 +2,7 @@
 pnpm run build:all
 rm -rf dist
 mkdir dist
+tree -I "node_modules"
 cp packages/comment-box-advance/dist/**/*.js dist
 cp packages/comment-box-advance/dist/**/*.css dist
 cp packages/comment-box-lite/dist/**/*.js dist
