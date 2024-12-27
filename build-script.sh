5fcf5560f8ce490f225c9df0df9c5f62ea76e9eb
w4ngzhen <w4ngzhen@hotmail.com>
2024-12-27 10:09:09 +0800

    refactor: 调整编译脚本

--- build-script.sh
+++ build-script.sh
@@ -2,8 +2,9 @@
 pnpm run build:all
 rm -rf dist
 mkdir dist
-tree -I "node_modules"
-cp packages/comment-box-advance/dist/**/*.js dist
-cp packages/comment-box-advance/dist/**/*.css dist
-cp packages/comment-box-lite/dist/**/*.js dist
-cp packages/comment-box-lite/dist/**/*.css dist
+cp ./packages/comment-box-advance/dist/** dist
+cp ./packages/comment-box-advance/dist/** dist
+cp ./packages/comment-box-lite/dist/** dist
+cp ./packages/comment-box-lite/dist/** dist
+# remove html file.
+find ./dist -type f -name "*.html" -delete
